6a7e951e8266d9583a26efc47a0cec55c419a5b3
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-01-15 16:23:57 +0100

    Update __init__.py

--- soccer_xg/__init__.py
+++ soccer_xg/__init__.py
@@ -1,8 +1,10 @@
-"""A Python package for training and analyzing expected goals (xG) models in soccer."""
+"""
+A Python package for training and analyzing expected goals (xG) models in soccer.
+
+:copyright: (c) 2023 by DTAI KU Leuven.
+:license: Apache v2, see LICENSE for more details.
+"""
+from soccer_xg.xg import XGModel
 
 __version__ = '0.0.1'
-
-__all__ = ['DataApi', 'XGModel']
-
-from soccer_xg.api import DataApi
-from soccer_xg.xg import XGModel
+__all__ = ['XGModel']
